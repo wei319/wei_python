89a742849b8d13d91c33b104da4e3558e6c523fc
wei319 <113099009+wei319@users.noreply.github.com>
2022-09-15 11:29:54 +0200

    Update test_csv.py
    add one line

--- test_csv.py
+++ test_csv.py
@@ -4,3 +4,4 @@ with open('test.csv', 'w', newline='') as file:
     writer.writerow(["ID", "sex", "age"])
     writer.writerow([111, "male", 20])
     writer.writerow([222, "female", 30])
+    wirter.writerow([333, "female", 30])
